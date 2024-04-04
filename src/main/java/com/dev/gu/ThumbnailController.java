6adc7bccf0613d1973cd4d277bd95a3b5fd47e21
rnskqkd <rnskqkd@naver.com>
2024-04-04 16:39:38 +0900

    레디스를 활용한 진행률 조회

--- src/main/java/com/dev/gu/ThumbnailController.java
+++ src/main/java/com/dev/gu/ThumbnailController.java
@@ -2,6 +2,8 @@ package com.dev.gu;
 
 import com.dev.gu.dto.ApiResponse;
 import com.dev.gu.dto.ApiResponseCode;
+import com.dev.gu.exception.BadRequestException;
+import com.dev.gu.exception.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -20,21 +25,30 @@ import org.springframework.web.multipart.MultipartFile;
 public class ThumbnailController {
     final ThumbnailService thumbnailService;
 
-    @Operation(summary = "썸네일 변환")
-    @PostMapping(value = "/generate-thumbnails", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<ApiResponse<Void>> uploadAndConvert(
+    @Operation(summary = "썸네일 변환", description = "taskId 로 변환 작업 진행률 상태 확인 가능")
+    @PostMapping(value = "/generate-thumbnails/{taskId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<ApiResponse<String>> uploadAndConvert(
+            @PathVariable String taskId,
             @RequestPart(value = "file") MultipartFile file
     ) {
-        thumbnailService.generateThumbnails(file);
-        return ApiResponse.toResponseEntity(ApiResponseCode.RESPONSE_OK);
+        try {
+            CompletableFuture<String> future = thumbnailService.generateThumbnails(taskId, file);
+            String result = future.get();
+            return ApiResponse.toResponseEntity(ApiResponseCode.RESPONSE_OK, result);
+        } catch (ExecutionException | InterruptedException e) {
+            log.error(e.getMessage(), e);
+            throw new BadRequestException(ErrorCode.INVALID_FILE);
+        }
     }
 
     @Operation(summary = "변환 작업 진행률 확인")
-    @GetMapping("/progress")
-    public ResponseEntity<ApiResponse<String>> getProgress() {
+    @GetMapping("/progress/{taskId}")
+    public ResponseEntity<ApiResponse<String>> getProgress(
+            @PathVariable String taskId
+    ) {
         return ApiResponse.toResponseEntity(
                 ApiResponseCode.RESPONSE_OK,
-                thumbnailService.getProgress()
+                thumbnailService.getProgress(taskId)
         );
     }
 }
--- src/main/java/com/dev/gu/ThumbnailService.java
+++ src/main/java/com/dev/gu/ThumbnailService.java
@@ -6,7 +6,9 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
@@ -17,11 +19,14 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -44,15 +49,14 @@ public class ThumbnailService {
 
     final RedisTemplate<String, String> redisTemplate;
 
-    int totalImages = 0;
     int completedImages = 0;
 
     /**
      * Redis에서 진행률 가져오기
      */
-    public String getProgress() {
+    public String getProgress(String taskId) {
         return String.valueOf(
-                redisTemplate.opsForValue().get("thumbnail")
+                redisTemplate.opsForValue().get("thumbnail: " + taskId)
         );
     }
 
@@ -60,35 +64,82 @@ public class ThumbnailService {
      * 썸네일 만들기 및 진행률 저장
      */
     @Async
-    public void generateThumbnails(MultipartFile file) {
+    public CompletableFuture<String> generateThumbnails(String inputTaskId, MultipartFile file) {
+        String zipFileFolderName = FilenameUtils.removeExtension(file.getOriginalFilename()) + RandomStringUtils.randomNumeric(6);
+        Path unzipFolderPath = Paths.get(basePath + thumbnailPath + File.separator + zipFileFolderName);
+
+        List<String> filePaths = new ArrayList<>();
+        List<String> imageExtensions = Arrays.asList(".jpg", ".jpeg", ".png", ".gif", ".bmp");
+
+        /* 초기화 */
+        int totalImages = 0;
+        completedImages = 0;
+        int progress = 0;
+        String taskId = inputTaskId;
+//        String taskId = file.getOriginalFilename() + "_" + inputTaskId;
+
         try (ZipInputStream zipIn = new ZipInputStream(file.getInputStream())) {
             ZipEntry entry = zipIn.getNextEntry();
 
-            /* 전체 이미지 갯수 반영 */
-            if (entry != null)
-                totalImages = (int) entry.getSize();
-
-            /* 압축 파일 내의 각 항목에 대해 반복. */
+            /* 압축 파일 내의 각 항목에 대해 반복하여 파일의 총 개수를 계산 */
             while (entry != null) {
-                String filePath = basePath + File.separator + thumbnailPath + File.separator + entry.getName();
-                /* 압축 파일 처리 및 이미지 변환. */
-                extractFile(zipIn, filePath);
-                zipIn.closeEntry();
+                String fileExt = entry.getName().toLowerCase();
 
-                /* 완료 갯수 반영 */
-                completedImages++;
+                if (imageExtensions.stream().anyMatch(fileExt::endsWith)) {
+                    // 이미지 파일인 경우에만 개수를 증가시킴
+                    ++totalImages;
+                }
 
+                String filePath = basePath + thumbnailPath + File.separator +
+                        zipFileFolderName + File.separator + entry.getName();
+
+                if (!Files.exists(unzipFolderPath)) {
+                    Files.createDirectories(unzipFolderPath);
+                }
+
+                /* 압축해제 */
+                unzipImage(zipIn, filePath);
+
+                filePaths.add(filePath);
                 entry = zipIn.getNextEntry();
             }
+
+            /* 초기화 */
+            redisTemplate.opsForValue().set(
+                    "thumbnail: " + taskId,
+                    totalImages + "개 이미지가 있음, " + completedImages + "개 변환된 상태, " + progress + "% 출력",
+                    Duration.ofHours(12)
+            );
+
+            redisTemplate.opsForValue().set(
+                    "thumbnail_totalImages: " + taskId,
+                    String.valueOf(totalImages),
+                    Duration.ofHours(12)
+            );
+
+            redisTemplate.opsForValue().set(
+                    "thumbnail_completedImages: " + taskId,
+                    String.valueOf(completedImages),
+                    Duration.ofHours(12)
+            );
+
+            /* 변환 */
+            extractFile(filePaths, taskId);
+
+            /* 변환 완료 된 폴더 삭제 */
+            File unzipFolder = new File(String.valueOf(unzipFolderPath));
+            FileUtils.cleanDirectory(unzipFolder);
+            FileUtils.delete(unzipFolder);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
+        return CompletableFuture.completedFuture(taskId);
     }
 
     /**
-     * 압축 파일 처리 및 이미지 변환
+     * 압축해제
      */
-    private void extractFile(ZipInputStream zipIn, String filePath) {
+    private void unzipImage(ZipInputStream zipIn, String filePath) {
         try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath))) {
             byte[] bytesIn = new byte[4096];
             int read = 0;
@@ -98,64 +149,102 @@ public class ThumbnailService {
         } catch (Exception e) {
             log.error(e.getMessage());
         }
+    }
 
-        File file = new File(filePath);
-        ProcessBuilder builder = new ProcessBuilder();
-        Path thumbnailFolder = Path.of(basePath, thumbnailPath);
-        String outputFile = thumbnailFolder.resolve(FilenameUtils.removeExtension(file.getAbsolutePath()) + ".webp").toAbsolutePath().toString();
-        List<Process> processes = new ArrayList<>();
+    /**
+     * 압축 처리된 폴더 내 이미지 파일 변환
+     */
+    private void extractFile(List<String> filePaths, String taskId) {
+        for (String filePath : filePaths) {
+            File file = new File(filePath);
+            ProcessBuilder builder = new ProcessBuilder();
+            Path thumbnailFolderPath = Path.of(basePath, thumbnailPath);
+            List<Process> processes = new ArrayList<>();
 
-        String[] defaultCmd = new String[]{
-                getCWebpPath(),
-                "-mt",
-                "-q", "50", // 압축률 50%
-        };
-        String[] defaultGifCmd = new String[]{
-                getCWebpPath(false),
-                "-mt",
-                "-lossy",
-        };
-
-        boolean isGif = file.getName().endsWith(".gif");
-
-        List<String> cmd = new ArrayList<>(List.of(isGif ? defaultGifCmd : defaultCmd));
-
-        builder.directory(file.getParentFile());
-        cmd.add(file.getName());
-        cmd.add("-o");
-        cmd.add(outputFile);
-        builder.command(cmd);
-
-        try {
-            log.info("BUILDER: {}", builder.directory());
-            log.info("COMMAND: {}", builder.command());
-            Process process = builder.start();
-            processes.add(process);
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-            throw new BadRequestException(ErrorCode.INVALID_FILE);
-        }
-
-        for (Process prc : processes) {
             try {
-                prc.waitFor();
-                prc.destroy();
+                if (!Files.exists(thumbnailFolderPath)) {
+                    Files.createDirectories(thumbnailFolderPath);
+                }
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            }
+
+            String outputFile = thumbnailFolderPath.resolve(
+                    FilenameUtils.removeExtension(thumbnailFolderPath + File.separator + file.getName()) + ".webp"
+            ).toAbsolutePath().toString();
+
+            String[] defaultCmd = new String[]{
+                    getCWebpPath(),
+                    "-mt",
+                    "-q", "50", // 압축률 50%
+            };
+            String[] defaultGifCmd = new String[]{
+                    getCWebpPath(false),
+                    "-mt",
+                    "-lossy",
+            };
+
+            boolean isGif = file.getName().endsWith(".gif");
+
+            List<String> cmd = new ArrayList<>(List.of(isGif ? defaultGifCmd : defaultCmd));
+
+            builder.directory(file.getParentFile());
+            cmd.add(file.getName());
+            cmd.add("-o");
+            cmd.add(outputFile);
+            builder.command(cmd);
+
+            try {
+                log.info("BUILDER: {}", builder.directory());
+                log.info("COMMAND: {}", builder.command());
+                Process process = builder.start();
+                processes.add(process);
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+                throw new BadRequestException(ErrorCode.INVALID_FILE);
+            }
+
+            for (Process prc : processes) {
+                try {
+                    prc.waitFor();
+                    prc.destroy();
+                } catch (InterruptedException e) {
+                    // do nothing
+                }
+            }
+
+            /* 변환 진행률을 Redis에 저장. */
+            int totalImages = Integer.parseInt(String.valueOf(
+                    redisTemplate.opsForValue().get("thumbnail_totalImages: " + taskId)
+            ));
+
+            completedImages = Integer.parseInt(String.valueOf(
+                    redisTemplate.opsForValue().get("thumbnail_completedImages: " + taskId)
+            ));
+
+            completedImages++;
+
+            redisTemplate.opsForValue().set(
+                    "thumbnail_completedImages: " + taskId,
+                    String.valueOf(completedImages),
+                    Duration.ofHours(12)
+            );
+
+            int progress = (int)(((double)completedImages / totalImages) * 100);
+
+            redisTemplate.opsForValue().set(
+                    "thumbnail: " + taskId,
+                    totalImages + "개 이미지가 있음, " + completedImages + "개 변환된 상태, " + progress + "% 출력",
+                    Duration.ofHours(12)
+            );
+
+            /* delay */
+            try {
+                Thread.sleep(500);
             } catch (InterruptedException e) {
-                // do nothing
+                log.error(e.getMessage());
             }
         }
-
-        /* 변환 완료 된 파일 삭제 */
-        file.delete();
-
-        /* 변환 진행률을 Redis에 저장. */
-        int progress = (int)(((double)completedImages / totalImages) * 100);
-
-        redisTemplate.opsForValue().set(
-                "thumbnail",
-                completedImages + "개 이미지가 있음, " + completedImages + "개 변환된 상태, " + progress + "% 출력",
-                Duration.ofHours(1)
-        );
     }
 
     /**
